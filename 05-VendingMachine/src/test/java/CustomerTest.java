2f11b1b8185fa04648cf762d6f2e7eb3f1bd6f9f
sogoagain <imyong0@gmail.com>
2019-05-25 21:52:44 +0900

    TDD05(자판기)_Customer 테스트 작성

--- 05-VendingMachine/src/test/java/CustomerTest.java
+++ 05-VendingMachine/src/test/java/CustomerTest.java
@@ -1,14 +1,34 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class CustomerTest {
 
+    private int INITIAL_FUNDS = 10_000;
     private Customer customer;
 
+    @Before
+    public void initialize() {
+        customer = new Customer(INITIAL_FUNDS);
+    }
+
     @Test
-    public void test_CustomerTest() throws Exception {
-        VendingMachine vavendingMachine = VendingMachine.createMachine();
-        customer.putMoney(vendingMachine);
+    public void 손님은_자판기에서_음료를_구매할_수_있다() throws Exception {
+        customer.buyBeverage("콜라", cokeVendingMachine);
+        boolean expectedResult = customer.hasBeverage("콜라");
+        assertThat(expectedResult).isEqualTo(true);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void 자판기에_없는_음료는_구매할_수_없다() throws Exception {
+        customer.buyBeverage("사이다", cokeVendingMachine);
+    }
+
+    @Test
+    public void 손님은_음료를_구매하면_잔액이_줄어든다() throws Exception {
+        customer.buyBeverage("콜라", cokeVendingMachine);
+        int change = customer.getMoney();
+        assertThat(change).isEqualTo(INITIAL_FUNDS - 1_000);
     }
 }
